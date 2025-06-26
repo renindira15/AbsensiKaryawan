1a738dd55daa0724687537abf54650ce4384bcf6
renindira15 <reninta.judith.prameswari-2022@ftmm.unair.ac.id>
2025-06-26 19:09:53 +0700

    Add files via upload

--- firebase_config.py
+++ firebase_config.py
@@ -5,7 +5,7 @@ from firebase_admin import credentials, db
 @st.cache_resource
 def init_firebase():
     if not firebase_admin._apps:
-        cred = credentials.Certificate(st.secrets["firebase_credentials"])
+        cred = dict(st.secrets["firebase_credentials"])
         firebase_admin.initialize_app(cred, {
             "databaseURL": "https://cloud-computing-1ce46-default-rtdb.asia-southeast1.firebasedatabase.app"
         })
